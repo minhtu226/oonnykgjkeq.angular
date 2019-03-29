555303ff94e152544d52a10cb729c409256235a5
minhtu226 <minhtu226iuh@gmail.com>
2019-03-29 10:39:23 +0700

    Initial commit

---
+++ app/progress-bar-determinate-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Determinate progress-bar
+ */
+@Component({
+  selector: 'progress-bar-determinate-example',
+  templateUrl: 'progress-bar-determinate-example.html',
+  styleUrls: ['progress-bar-determinate-example.css'],
+})
+export class ProgressBarDeterminateExample {}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
